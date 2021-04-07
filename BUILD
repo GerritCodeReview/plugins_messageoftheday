409c190995b9e2b8a3c6713361aa710c9183bb04
paladox <thomasmulhall410@yahoo.com>
2021-04-07 22:21:56 +0100

    Convert to Polymer 3
    Change-Id: I96805eb374613b7657cd19c5de5f284d3e978c40

--- BUILD
+++ BUILD
@@ -10,5 +10,36 @@ gerrit_plugin(
         "Implementation-Title: Plugin messageoftheday",
         "Implementation-URL: https://gerrit-review.googlesource.com/#/admin/projects/plugins/messageoftheday",
     ],
+    resource_jars = [":gr-messageoftheday-static"],
     resources = glob(["src/main/resources/**/*"]),
+)
+
+genrule2(
+    name = "zuul-status-static",
+    srcs = [":gr-messageoftheday"],
+    outs = ["gr-messageoftheday-static.jar"],
+    cmd = " && ".join([
+        "mkdir $$TMP/static",
+        "cp -r $(locations :gr-messageoftheday) $$TMP/static",
+        "cd $$TMP",
+        "zip -Drq $$ROOT/$@ -g .",
+    ]),
+)
+
+polygerrit_plugin(
+    name = "gr-messageoftheday",
+    app = "gr-messageoftheday-bundle.js",
+    plugin_name = "gr-messageoftheday",
+)
+
+rollup_bundle(
+    name = "gr-messageoftheday-bundle",
+    srcs = glob(["gr-messageoftheday/*.js"]),
+    entry_point = "gr-messageoftheday/plugin.js",
+    format = "iife",
+    rollup_bin = "//tools/node_tools:rollup-bin",
+    sourcemap = "hidden",
+    deps = [
+        "@tools_npm//rollup-plugin-node-resolve",
+    ],
 )
---
+++ gr-messageoftheday/gr-messageoftheday-banner.js
@@ -0,0 +1,64 @@
+/**
+ * @license
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {htmlTemplate} from './gr-messageoftheday-banner_html.js';
+
+class GrMessageOfTheDayBanner extends Polymer.Element {
+  static get is() {
+    return 'gr-messageoftheday-banner';
+  }
+
+  static get template() {
+    return htmlTemplate;
+  }
+
+  static get properties() {
+    return {
+      _message: Object,
+      _hidden: {
+        type: Boolean,
+        value: true,
+      }
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.plugin.restApi()
+      .get(`/config/server/${this.plugin.getPluginName()}~message`)
+      .then(message => {
+        if (!message || !message.html) {
+          return;
+        }
+        this._message = message;
+        this._isHidden();
+        this.$.message.innerHTML = this._message.html;
+      });
+  }
+
+  _handleDismissMessage() {
+    document.cookie =
+      `msg-${this._message.id}=1; expires=${this._message.redisplay}`;
+    this._hidden = true;
+  }
+
+  _isHidden() {
+    this._hidden = window.util.getCookie(`msg-${this._message.id}`) === '1';
+  }
+}
+
+customElements.define(GrMessageOfTheDayBanner.is, GrMessageOfTheDayBanner);
