8b3535918384df16a47e9a039b1b782462e60b84
Kaushik Lingarkar <kaushik.lingarkar@linaro.org>
2024-08-16 10:51:17 -0700

    Allow configuring config and data dirs
    This change allows the plugin config and data directories to
    be configured. It lays the foundation for further extending
    the configuration in a subsequent change, where the plugin
    config and content can be read from a repository.
    
    Change-Id: I3844dc9183eb84566b3b9bece3a9549b035984db

--- src/main/java/com/googlesource/gerrit/plugins/messageoftheday/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/messageoftheday/Module.java
@@ -16,10 +16,29 @@ package com.googlesource.gerrit.plugins.messageoftheday;
 
 import static com.google.gerrit.server.config.ConfigResource.CONFIG_KIND;
 
+import com.google.gerrit.extensions.annotations.PluginData;
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import java.nio.file.Path;
 
 class Module extends AbstractModule {
+  private static final String CONFIG_DIR = "configDir";
+  private static final String DATA_DIR = "dataDir";
+  private final SitePaths sitePaths;
+  private final PluginConfigFactory cfg;
+
+  @Inject
+  public Module(SitePaths sitePaths, PluginConfigFactory cfg) {
+    this.sitePaths = sitePaths;
+    this.cfg = cfg;
+  }
+
   @Override
   protected void configure() {
     install(
@@ -29,5 +48,21 @@ class Module extends AbstractModule {
             get(CONFIG_KIND, "message").to(GetMessage.class);
           }
         });
+  }
+
+  @Provides
+  @Singleton
+  @ConfigDir
+  Path provideConfigDir(@PluginName String pluginName) {
+    String configDir = cfg.getFromGerritConfig(pluginName).getString(CONFIG_DIR);
+    return configDir != null ? Path.of(configDir) : sitePaths.etc_dir;
+  }
+
+  @Provides
+  @Singleton
+  @DataDir
+  Path provideDataDir(@PluginName String pluginName, @PluginData Path dataDirPath) {
+    String dataDir = cfg.getFromGerritConfig(pluginName).getString(DATA_DIR);
+    return dataDir != null ? Path.of(dataDir) : dataDirPath;
   }
 }
